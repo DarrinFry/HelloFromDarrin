5ba4f9e23360fc83f097b048aaadfc15faea4805
Darrin <productionstagemanager@gmail.com>
2020-10-07 22:26:33 -0600

    Thanks Guys. I have seen your updates.

--- src/com/Fry/Darrin/Main.java
+++ src/com/Fry/Darrin/Main.java
@@ -7,5 +7,8 @@ public class Main {
         System.out.println("Hello, my name is Darrin");
         System.out.println("Hello Darrin, my name is Jacob!");
         System.out.println("Hello Darrin, my name is Freddy");
+
+        //I have seen your updates.
+        System.out.println("Thanks everyone. You guys are the greatest.");
     }
 }
